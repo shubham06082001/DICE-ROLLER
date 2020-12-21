103700c7bd172828b449b73fcd3b3a1d26a52bec
SHUBHAM KUMAR <shubhamkmr06082001@gmail.com>
2020-12-21 18:43:15 +0530

    Initial Commit

---
+++ DICE ROLLER/DICE_ROLLERApp.swift
@@ -0,0 +1,17 @@
+//
+//  DICE_ROLLERApp.swift
+//  DICE ROLLER
+//
+//  Created by SHUBHAM KUMAR on 21/12/20.
+//
+
+import SwiftUI
+
+@main
+struct DICE_ROLLERApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
