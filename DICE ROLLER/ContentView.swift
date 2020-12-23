e82377534470a069358fa56264b20503bdb70dc1
SHUBHAM KUMAR <shubhamkmr06082001@gmail.com>
2020-12-23 18:01:51 +0530

    UPDATED!!

--- DICE ROLLER/ContentView.swift
+++ DICE ROLLER/ContentView.swift
@@ -8,9 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var left = 1
+    @State var right = 2
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Image("background")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .edgesIgnoringSafeArea(.all)
+                .scaledToFill()
+            VStack {
+                Image("diceeLogo")
+                Spacer()
+                HStack {
+                    DiceView(n: left)
+                    DiceView(n: right)
+                }
+                Spacer()
+                Button(action: {
+                    self.left = Int.random(in: 1...6)
+                    self.right = Int.random(in: 1...6)
+                }) {
+                    Text("ROLL")
+                        .font(.system(size: 50))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                        .padding(.horizontal)
+                        .padding(.vertical)
+                }
+                .background(Color.red)
+            }
+        }
+    }
+}
+
+struct DiceView: View {
+    let n: Int
+    var body: some View {
+        Image("dice\(n)")
+            .resizable()
+            .aspectRatio(1, contentMode: .fit).frame(width: 150, height: 150)
+            .padding(10)
+        
     }
 }
 
@@ -19,3 +59,5 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
